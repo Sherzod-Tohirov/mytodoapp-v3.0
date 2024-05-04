e73836cf6b5a1a23fa0f340ec57e80048a80b1a2
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2024-05-04 14:43:02 +0500

    Add some sound effects

--- src/components/Item/hooks/useHandleCompleteTask.tsx
+++ src/components/Item/hooks/useHandleCompleteTask.tsx
@@ -3,9 +3,9 @@ import { todo } from "../../../utils/types";
 import { useDispatch } from "react-redux";
 import { editIsCompleted } from "../../../store/todo/todoSlice";
 import toast from "react-hot-toast";
-
+import completedAudioSrc from '../../../assets/audio/completed2.mp3';
 export function useHandleCompleteTask(data: todo) {
-  
+  const completedAudio = new Audio(completedAudioSrc);
   const [completed, setCompleted] = useState(data.isCompleted);
   const [completedChanged, setCompletedChanged] = useState(false);
   const dispatch = useDispatch();
@@ -14,6 +14,7 @@ export function useHandleCompleteTask(data: todo) {
     dispatch(editIsCompleted({ id: data.id, isCompleted: e.target.checked }));
     setCompleted((prev) => !prev);
     if (!completed) {
+      completedAudio.play();
       toast.success("You completed task !");
     } else toast.success("You undone task !");
   }
